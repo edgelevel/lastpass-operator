09af894324d4a46efbb73820178621dee944efad
niqdev <niqdev@gmail.com>
2019-07-11 17:02:04 +0100

    update travis deploy

---
+++ .travis/install_operator_sdk.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# see https://github.com/operator-framework/operator-sdk/blob/master/doc/user/install-operator-sdk.md
+
+export OPERATOR_SDK_VERSION=v0.8.1
+
+echo "[+] Setup operator-sdk"
+
+curl -OJL https://github.com/operator-framework/operator-sdk/releases/download/${OPERATOR_SDK_VERSION}/operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
+chmod +x operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu
+sudo mv operator-sdk-${OPERATOR_SDK_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk
+
+echo "[-] Setup operator-sdk"
