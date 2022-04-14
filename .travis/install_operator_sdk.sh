9b833073233a45dd4d527d3ae5773c3a84f24cdf
Paul Laffitte <paul.laffitte@enix.fr>
2022-04-14 12:57:56 +0200

    upgrade to operator-sdk 1.0.1 and crdVersion v1

--- .travis/install_operator_sdk.sh
+++ .travis/install_operator_sdk.sh
@@ -2,7 +2,7 @@
 
 # see https://github.com/operator-framework/operator-sdk/blob/master/doc/user/install-operator-sdk.md
 
-export OPERATOR_SDK_VERSION=v0.18.2
+export OPERATOR_SDK_VERSION=v1.0.1
 
 echo "[+] Setup operator-sdk"
 
